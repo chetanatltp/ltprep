bef490b6d501f14c520d97cb3ba156bcc37782ab
CS <chetanatltp@gmail.com>
2017-04-19 16:11:05 +0530

    committed bot.js and index.html - first version

---
+++ bot.js
@@ -0,0 +1,43 @@
+var express = require('express');
+var methodOverride = require('method-override');
+var bodyParser = require('body-parser');
+var path = require('path');
+
+var app = express();
+
+var apiai = require('apiai');
+var api = apiai("6058192092c24cb98992128bb92d0f15");
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.static(path.join(__dirname)));
+
+//THis is the ask question api
+app.post("/ask", function(req, res) {
+    var question = req.body.question;
+    var request = api.textRequest(question, {
+        sessionId: 'LTP1234'
+    });
+
+    request.on('response', function(response) {
+        console.log(response);
+        res.status(200).send({
+            data: response.result.fulfillment.speech,
+            status: 'ok'
+        });
+    });
+    
+    request.on('error', function(error) {
+        console.log(error);
+        res.status(500).send({
+            data: 'Could not get response from apiai',
+            status: 'failed'
+        });
+    });
+    
+    request.end();
+});
+
+app.listen(5000, function() {
+    console.log("Listening on " + (5000));
+});
